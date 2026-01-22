e2da426ba12ee6330e728f058e20091af78d7d77
Mayank Kumar <mayank.91986@gmail.com>
2026-01-22 00:00:46 +0000

    optimizing the app

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", exact: "1.5.0"),
         .package(url: "https://github.com/groue/GRDB.swift.git", from: "6.24.0")
     ],
     targets: [
